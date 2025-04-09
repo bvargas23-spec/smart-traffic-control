099761203aeb1c91dba850b579b94aa1f6bb047c
bvargas23-spec <brianadonai123@gmail.com>
2025-04-09 15:14:36 -0400

    Refactor intersection traffic light control to simplify GPIO setup and sequence

--- tests/intersection_test.py
+++ tests/intersection_test.py
@@ -1,65 +1,45 @@
 import Adafruit_BBIO.GPIO as GPIO
 import time
 
-# First traffic light (North) - pins you're already using
-NORTH_RED_PIN = "P8_11"
-NORTH_YELLOW_PIN = "P8_12"
-NORTH_GREEN_PIN = "P8_15"
+# Set up the GPIO pins
+# Using the pins from the PRU-ICSS table
+RED_PIN = "P8_11"    # GPIO1_13
+YELLOW_PIN = "P8_12" # GPIO1_12 
+GREEN_PIN = "P8_15"  # GPIO1_15
 
-# Second traffic light (East) - new pins
-EAST_RED_PIN = "P8_7"
-EAST_YELLOW_PIN = "P8_8"
-EAST_GREEN_PIN = "P8_9"
-
-# Set up all pins as outputs
-GPIO.setup(NORTH_RED_PIN, GPIO.OUT)
-GPIO.setup(NORTH_YELLOW_PIN, GPIO.OUT)
-GPIO.setup(NORTH_GREEN_PIN, GPIO.OUT)
-GPIO.setup(EAST_RED_PIN, GPIO.OUT)
-GPIO.setup(EAST_YELLOW_PIN, GPIO.OUT)
-GPIO.setup(EAST_GREEN_PIN, GPIO.OUT)
+# Set up the pins as outputs
+GPIO.setup(RED_PIN, GPIO.OUT)
+GPIO.setup(YELLOW_PIN, GPIO.OUT)
+GPIO.setup(GREEN_PIN, GPIO.OUT)
 
 def all_off():
     """Turn all lights off"""
-    GPIO.output(NORTH_RED_PIN, GPIO.LOW)
-    GPIO.output(NORTH_YELLOW_PIN, GPIO.LOW)
-    GPIO.output(NORTH_GREEN_PIN, GPIO.LOW)
-    GPIO.output(EAST_RED_PIN, GPIO.LOW)
-    GPIO.output(EAST_YELLOW_PIN, GPIO.LOW)
-    GPIO.output(EAST_GREEN_PIN, GPIO.LOW)
+    GPIO.output(RED_PIN, GPIO.LOW)
+    GPIO.output(YELLOW_PIN, GPIO.LOW)
+    GPIO.output(GREEN_PIN, GPIO.LOW)
 
 try:
-    # Run the intersection traffic light sequence
-    print("Starting intersection control. Press Ctrl+C to stop.")
+    # Run the traffic light sequence
+    print("Starting traffic light sequence. Press Ctrl+C to stop.")
     
     while True:
-        # North green, East red
+        # Red light
         all_off()
-        print("North: GREEN, East: RED")
-        GPIO.output(NORTH_GREEN_PIN, GPIO.HIGH)
-        GPIO.output(EAST_RED_PIN, GPIO.HIGH)
-        time.sleep(5)
+        print("RED ON")
+        GPIO.output(RED_PIN, GPIO.HIGH)
+        time.sleep(3)
         
-        # North yellow, East red
+        # Green light
         all_off()
-        print("North: YELLOW, East: RED")
-        GPIO.output(NORTH_YELLOW_PIN, GPIO.HIGH)
-        GPIO.output(EAST_RED_PIN, GPIO.HIGH)
-        time.sleep(2)
+        print("GREEN ON")
+        GPIO.output(GREEN_PIN, GPIO.HIGH)
+        time.sleep(3)
         
-        # North red, East green
+        # Yellow light
         all_off()
-        print("North: RED, East: GREEN")
-        GPIO.output(NORTH_RED_PIN, GPIO.HIGH)
-        GPIO.output(EAST_GREEN_PIN, GPIO.HIGH)
-        time.sleep(5)
-        
-        # North red, East yellow
-        all_off()
-        print("North: RED, East: YELLOW")
-        GPIO.output(NORTH_RED_PIN, GPIO.HIGH)
-        GPIO.output(EAST_YELLOW_PIN, GPIO.HIGH)
-        time.sleep(2)
+        print("YELLOW ON")
+        GPIO.output(YELLOW_PIN, GPIO.HIGH)
+        time.sleep(1)
         
 except KeyboardInterrupt:
     print("\nTraffic light sequence stopped by user")
