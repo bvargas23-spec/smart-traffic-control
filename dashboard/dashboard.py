d9014fe34fcabb31206c7f53012e87aa2571853a
bvargas23-spec <brianadonai123@gmail.com>
2025-04-27 15:22:18 -0400

    Update STATUS_TOPIC to specify status updates for traffic intersections

--- dashboard/dashboard.py
+++ dashboard/dashboard.py
@@ -31,7 +31,7 @@ PRIVATE_KEY_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "cer
 ROOT_CA_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "certs/AmazonRootCA1.pem")
 
 # MQTT Topics
-STATUS_TOPIC = "traffic/intersection/#"
+STATUS_TOPIC = "traffic/intersection/status/#"
 TIMING_TOPIC = "traffic/intersection/timing/#"
 CONGESTION_TOPIC = "traffic/#"
 
